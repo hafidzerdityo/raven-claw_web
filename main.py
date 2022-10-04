ed8a81cceb0beec11c4f27040fbeade6451d5581
Hafidz Erdityo <hafidze15@gmail.com>
2022-10-04 22:20:21 +0700

    fix 10

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from dotenv import load_dotenv
 import os
 
 
-# load_dotenv()
+load_dotenv()
 
-# mongo_key = os.getenv("MONGO_PASS")
-# # connection_string = "mongodb://localhost:27017"  # local
+mongo_key = os.getenv("MONGO_PASS")
+# connection_string = "mongodb://localhost:27017"  # local
 
-# # mongo db cloud
-# connection_string = f'mongodb+srv://hafidz_erdityo:{mongo_key}@cluster0.uorwk7c.mongodb.net/?retryWrites=true&w=majority'
-# client = pymongo.MongoClient(connection_string)
+# mongo db cloud
+connection_string = f'mongodb+srv://hafidz_erdityo:{mongo_key}@cluster0.uorwk7c.mongodb.net/?retryWrites=true&w=majority'
+client = pymongo.MongoClient(connection_string)
 
-# print(client)
+print(client)
 
 app = FastAPI()
 origins = ["*"]
@@ -47,15 +47,15 @@ def encryption(user_pass):
     return hashlib.sha512(user_pass.encode()).hexdigest()
 
 
-# def find_data(p_username):
-#     query = {
-#         'user_name': p_username
-#     }
-#     return client['pos_cp']['login_data'].find_one(query)
+def find_data(p_username):
+    query = {
+        'user_name': p_username
+    }
+    return client['pos_cp']['login_data'].find_one(query)
 
 
-# def ingest_regist(p_data):
-#     client['pos_cp']['login_data'].insert_one(p_data)
+def ingest_regist(p_data):
+    client['pos_cp']['login_data'].insert_one(p_data)
 
 
 @app.get("/")
