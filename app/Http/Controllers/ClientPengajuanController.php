812670068856c1e7d8c1e15982fed31fffd2a4b5
winarwahyuw <winarwahyuw99@gmail.com>
2022-10-05 12:31:06 +0700

    updated untuk pisah login client admin

---
+++ app/Http/Controllers/ClientPengajuanController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientPengajuanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('client.list_pengajuan');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('client.form_pengajuan');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $response = Http::post('https://ravenclaw-services.herokuapp.com/CRUD/client/form-pengajuan', [
+            'username' => $request->username,
+            'name' => $request->name,
+            'divisi' => $request->divisi,
+            'aktivitas' => $request->aktivitas,
+            'due_date' => $request->due_date,
+            'bsu_fix' => $request->bsu_fix
+        ]);
+
+        $result = \json_decode($response, true);
+        
+        if($result['status']=='success'){
+            return redirect()->route('client_list_pengajuan');
+        }else{
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/client/form_pengajuan.blade.php
@@ -0,0 +1,176 @@
+@extends('template.master')
+
+@section('main-content')
+          <!-- Page Content -->
+  <main id="main">
+
+    <!-- Content-->
+    <section class="container-fluid">
+
+      <!-- Breadcrumbs-->
+      <nav class="mb-4 pb-2 border-bottom" aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="./index.html"><i class="ri-home-line align-bottom me-1"></i> Tambah Pengajuan</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Tables</li>
+          </ol>
+      </nav>      <!-- / Breadcrumbs-->
+
+      <div class="row g-4">
+        <div class="col-9">
+            <div class="card mb-4">
+                <div class="card-header justify-content-between align-items-center d-flex">
+                    <h6 class="card-title m-0">Default form</h6>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                aria-describedby="emailHelp" placeholder="Enter your email">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+      </div>
+      
+      <!-- Sidebar Menu Overlay-->
+      <div class="menu-overlay-bg"></div>
+      <!-- / Sidebar Menu Overlay-->
+      
+      <!-- Modal Imports-->
+      <!-- Place your modal imports here-->
+      
+      <!-- Default Example Modal Import-->
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                Here goes modal body content
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      <!-- Offcanvas Imports-->
+      <!-- Place your offcanvas imports here-->
+      
+      <!-- Default Example Offcanvas Import-->
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <div>
+              Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+            </div>
+            <div class="dropdown mt-3">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+                Dropdown button
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      <!-- Navbar Notifications offcanvas-->
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNotifications"
+        aria-labelledby="offcanvasNotificationsLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNotificationsLabel">Notifications</h5>
+          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+      
+          <!-- Notification-->
+          <div class="d-flex justify-content-start align-items-start p-3 rounded bg-light mb-3">
+            <div class="position-relative mt-1 ">
+              <picture class="avatar avatar-sm">
+                <img src="./assets/images/profile-small-2.jpeg"
+                  alt="HTML Bootstrap Admin Template by Pixel Rocket">
+              </picture>
+              <span
+              class="dot bg-success avatar-dot border-light dot-sm"></span>
+            </div>
+            <div class="ms-4">
+              <p class="fw-bolder mb-1">John Jackson</p>
+              <p class="text-muted small mb-0">Just sent over regional sales. If you can let me know by the end...</p>
+              <span class="fs-xs fw-bolder text-muted text-uppercase">5 mins ago</span>
+            </div>
+          </div>
+          <!-- / Notification-->
+      
+          <!-- Notification-->
+          <div class="d-flex justify-content-start align-items-start p-3 rounded bg-light mb-3">
+            <div class="position-relative mt-1 ">
+              <picture class="avatar avatar-sm">
+                <img src="./assets/images/profile-small-3.jpeg"
+                  alt="HTML Bootstrap Admin Template by Pixel Rocket">
+              </picture>
+              <span
+              class="dot bg-success avatar-dot border-light dot-sm"></span>
+            </div>
+            <div class="ms-4">
+              <p class="fw-bolder mb-1">Peter Smith</p>
+              <p class="text-muted small mb-0">Hi Rob, can we setup a meeting for tomorrow around 2pm...</p>
+              <span class="fs-xs fw-bolder text-muted text-uppercase">30 mins ago</span>
+            </div>
+          </div>
+          <!-- / Notification-->
+      
+          <!-- Notification-->
+          <div class="d-flex justify-content-start align-items-start p-3 rounded bg-light mb-3">
+            <div class="position-relative mt-1 ">
+              <picture class="avatar avatar-sm">
+                <img src="./assets/images/profile-small-4.jpeg"
+                  alt="HTML Bootstrap Admin Template by Pixel Rocket">
+              </picture>
+              <span
+              class="dot bg-danger avatar-dot border-light dot-sm"></span>
+            </div>
+            <div class="ms-4">
+              <p class="fw-bolder mb-1">Helen Lewis</p>
+              <p class="text-muted small mb-0">Need to arrange for this year's Office lisences. Have to add two team licenses...</p>
+              <span class="fs-xs fw-bolder text-muted text-uppercase">43 mins ago</span>
+            </div>
+          </div>
+          <!-- / Notification-->
+      
+          <!-- View all Btn-->
+          <a href="#" class="btn btn-outline-secondary w-100 mt-4" role="button">View all notifications</a>
+          <!-- / View all btn-->
+      
+        </div>
+      </div>      <!-- / Footer-->
+
+    </section>
+    <!-- / Content-->
+
+  </main>
+  <!-- /Page Content -->
+
+
+@endsection
